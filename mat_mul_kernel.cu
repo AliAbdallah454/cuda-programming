8154dcd8732e94d166130a08c05795971f2437eb
aliabdallah454 <alikhaledabdallah454@gmail.com>
2025-07-31 14:43:17 +0300

    removed helper functions from mat_mul_kernel.cu

--- mat_mul_kernel.cu
+++ mat_mul_kernel.cu
@@ -14,34 +14,6 @@ using namespace std;
 #define K 512
 #define BLOCK_DIM 32
 
-// void fill_matrix(float *matrix, int r, int c) {
-//     for (int i = 0; i < r; ++i)
-//         for (int j = 0; j < c; ++j)
-//             matrix[i * c + j] = rand() % 100; // Random number [0, 99]
-// }
-
-// void print_matrix(const char *name, float *matrix, int r, int c) {
-//     printf("%s:\n", name);
-//     for (int i = 0; i < r; ++i) {
-//         for (int j = 0; j < c; ++j)
-//             printf("%4f ", matrix[i * c + j]);
-//         printf("\n");
-//     }
-//     printf("\n");
-// }
-
-// void mat_mul_cpu(float *A, float *B, float *C, int m, int k, int n) {
-//     for (int i = 0; i < m; ++i) {
-//         for (int j = 0; j < n; ++j) {
-//             float sum = 0;
-//             for (int l = 0; l < k; ++l) {
-//                 sum += A[i * k + l] * B[j + l * n];
-//             }
-//             C[i * n + j] = sum;
-//         }
-//     }
-// }
-
 __global__ void mat_mul_gpu(float *A, float *B, float *C, int m, int k, int n) {
 
     int i = blockIdx.y * blockDim.y + threadIdx.y;
